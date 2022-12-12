0cd2815a18967f2ee076a6295763cd1f09d8e087
Brady Ryan <balr@umich.edu>
2022-12-12 09:58:04 -0500

    Added more data sets.

---
+++ R/aggregation.R
@@ -0,0 +1,9 @@
+#' Data set containing 7 clusters of varying shapes and sizes
+#'
+#' @format Columns x and y containing the x and y coordinates respectively, and a
+#' column containing the true cluster label
+#'
+#' @references https://github.com/Kchu/CURE-cluster-python
+#'
+
+"aggregation"
---
+++ R/threeClus.R
@@ -0,0 +1,9 @@
+#' Data set containing 3 spherical shaped clusters
+#'
+#' @format Columns x and y containing the x and y coordinates respectively, and a
+#' column containing the true cluster label
+#'
+#' @references https://github.com/Kchu/CURE-cluster-python - Data set named 3clus.txt here
+#'
+
+"threeClus"
