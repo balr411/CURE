1d0b41dd52f230dee8a3e267a3b333d596eb6ac7
Brady Ryan <balr@umich.edu>
2022-12-12 09:43:46 -0500

    Moved some data files to data/

---
+++ R/jain.R
@@ -0,0 +1,9 @@
+#' Data set containing 2 parabolic shaped clusters
+#'
+#' @format Columns x and y containing the x and y coordinates respectively, and a
+#' column containing the true cluster label
+#'
+#' @references https://github.com/Kchu/CURE-cluster-python
+#'
+
+"jain"
